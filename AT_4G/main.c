3f34b036fab0720a22b26880fb58fa6f509da007
ds18b20 <2017603273@qq.com>
2023-07-15 11:46:08 +0800

    function

--- AT_4G/main.c
+++ AT_4G/main.c
@@ -28,34 +28,29 @@
 #include "comport.h"
 #include "atcmd.h"
 
+#define CONFIG_DEBUG
+
+#ifdef  CONFIG_DEBUG
+#define dbg_print(format, args...) printf(format, ##args)
+#else 
+#define dbg_print(format, args...) do {} while(0)
+#endif
+
 #define    TIMEOUT  2
 
 
-//打印帮助信息
-void print_usage(char *program_name)
-{
-    printf("Usage:%s[OPTION]\n\n", program_name);
-    printf("-b[baudrate]:Select baud rate, for example 115200 and 9600.\n");
-    printf("-p[parity]:Select parity check, for example n N e E o O.\n");
-    printf("-s[stopbits]:Select stop bit, for example 1 and 2.\n");
-    printf("-m[serial_name]:Select device file, for example /dev/ttyUSB0.\n");
-    printf("-h[help]:Printing Help Information.\n"); 
-    printf("For example:./SMS -b 115200 -p n -s 1 -m /dev/ttyUSB3 \n\n");
+void print_usage(char *program_name);
 
-}
+void sigusr1_handler(int signum);
 
-void sigusr1_handler(int signum)
-{
-    printf("Received SIGUSR1. Starting pppd...\n");
-    system("sudo pppd call rasppp");
-}
+void sigusr2_handler(int signum);
 
-void sigusr2_handler(int signum)
-{
-    printf("Received SIGUSR2. Stopping pppd...\n");
-    system("sudo poff rasppp");
-    exit(0);
-}
+void install_signal(void);
+
+void handler(int sig);
+
+
+int g_stop = 0;
 
 
 int main(int argc, char *argv[])
@@ -69,7 +64,7 @@ int main(int argc, char *argv[])
     char            recv_buf[128];
     fd_set          rdset;
 	pid_t           pid;
-	pid     =     fork();
+	pid          =  fork();
     comport_tty_t   comport_tty;
 	comport_tty_t  *comport_tty_ptr;
 	comport_tty_ptr = &comport_tty;
@@ -129,7 +124,7 @@ int main(int argc, char *argv[])
 	if(pid < 0)
 	{
 		printf("fork() create child process failure: %s\n", strerror(errno));
-        return -4;
+        return -1;
     }
 
     if(pid == 0)
@@ -137,7 +132,7 @@ int main(int argc, char *argv[])
         //注册信号处理函数
         signal(SIGUSR1, sigusr1_handler);
         signal(SIGUSR2, sigusr2_handler);
-        printf("Child process PID[%d] running...\n", getpid());
+        dbg_print("Child process PID[%d] running...\n", getpid());
         while(1)
         {
             sleep(1);
@@ -146,31 +141,32 @@ int main(int argc, char *argv[])
 
 	else
 	{
-        printf("Parent process PID[%d] running...\n", getpid());
+        dbg_print("Parent process PID[%d] running...\n", getpid());
 
 		if(0 == strlen(comport_tty_ptr->serial_name))
 		{
 			printf("Failed to obtain the device name!\n");
-			return -1;
+			return -2;
 		}
 
+		install_signal();
 
 		if(tty_open(comport_tty_ptr) < 0)
 		{
 			printf("Failed to open the device file");
-			return -2;
+			return -3;
 		}
 
 		if(tty_init(comport_tty_ptr) < 0)
 		{
 			printf("Failed to initialize the serial port\n");
-			return -3;
+			return -4;
 		}
 
 		if(check_sim_all(comport_tty_ptr) < 0)
 		{
 			printf(" SIM initialize failure!\n");
-			return -4;
+			return -5;
 		}
 
 		while(1)
@@ -183,13 +179,13 @@ int main(int argc, char *argv[])
 			if(rv_fd < 0)
 			{
 				printf("Select listening for file descriptor error: %s\n",strerror(errno));
-				rv = -4;
+				rv = -6;
 				goto CleanUp;
 			}
 			else if(0 == rv_fd)
 			{
 				printf("Select listening for file descriptor timeout!\n");
-				rv = -5;
+				rv = -7;
 				goto CleanUp;
 			}
 			else
@@ -204,10 +200,10 @@ int main(int argc, char *argv[])
 					if(tty_send(comport_tty_ptr, send_buf, strlen(send_buf)) < 0)
 					{
 						printf("Failed to send data through the serial port\n");
-						rv = -6;
+						rv = -8;
 						goto CleanUp;
 					}
-					printf("Succeeded in send data serial port data:%s\n", send_buf);
+					dbg_print("Send data:%s\n", send_buf);
 					fflush(stdin);//冲洗输入流
 				}
 				else if(FD_ISSET(comport_tty_ptr->fd, &rdset))//判断是否是串口文件描述符响应
@@ -217,10 +213,10 @@ int main(int argc, char *argv[])
 					if(tty_recv(comport_tty_ptr, recv_buf, sizeof(recv_buf), TIMEOUT) < 0)
 					{
 						printf("Failed to receive serial port data!\n");
-						rv = -7;
+						rv = -9;
 						goto CleanUp;
 					}
-					printf("Succeeded in receiving serial port data:%s\n", recv_buf);
+					printf("Receiving port data:%s\n", recv_buf);
 					fflush(stdout);//冲洗输出流
 				}
 			}
@@ -231,5 +227,86 @@ int main(int argc, char *argv[])
 CleanUp: 
     tty_close(comport_tty_ptr);
     return rv;
+}
+
+
+//打印帮助信息
+void print_usage(char *program_name)
+{
+    printf("Usage:%s[OPTION]\n\n", program_name);
+    printf("-b[baudrate]:Select baud rate, for example 115200 and 9600.\n");
+    printf("-p[parity]:Select parity check, for example n N e E o O.\n");
+    printf("-s[stopbits]:Select stop bit, for example 1 and 2.\n");
+    printf("-m[serial_name]:Select device file, for example /dev/ttyUSB0.\n");
+    printf("-h[help]:Printing Help Information.\n"); 
+    printf("For example:./SMS -b 115200 -p n -s 1 -m /dev/ttyUSB3 \n\n");
+
+}
+
+void sigusr1_handler(int signum)
+{
+    printf("Received SIGUSR1. Starting pppd...\n");
+    system("sudo pppd call rasppp");
+}
+
+void sigusr2_handler(int signum)
+{
+    printf("Received SIGUSR2. Stopping pppd...\n");
+    system("sudo poff rasppp");
+    exit(0);
+}
+
+//对信号进行处理
+void handler(int sig)
+{
+    switch(sig)
+    {
+        case SIGINT:
+        {
+            printf("Process captured SIGINT signal!\n");
+            g_stop = 1;
+            break;
+        }
+        case SIGTERM:
+        {
+            printf("Process captured SIGTERM signal!\n");
+            g_stop = 1;
+            break;
+        }
+        case SIGSEGV:
+        {
+            printf("Process captured SIGSEGV signal!\n");
+            g_stop = 1;
+            exit(0);
+            break;
+        }
+        case SIGPIPE:
+        {
+            printf("Process captured SIGPIPE signal!\n");
+            g_stop = 1;
+            break;
+        }
+        default:
+            break;
+    }
+
+    return ;
+}
+
+//注册信号
+void install_signal(void)
+{
+    struct sigaction sigact;
+
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+    sigact.sa_handler = handler;
+
+    sigaction(SIGINT, &sigact, 0);
+    sigaction(SIGTERM, &sigact, 0);
+    sigaction(SIGPIPE, &sigact, 0);
+    sigaction(SIGSEGV, &sigact, 0);
+
+    return ;
 }
 
