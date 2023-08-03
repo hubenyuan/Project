3384c3f667d9e6997d0988e2f722e9bccc98d468
ds18b20 <2017603273@qq.com>
2023-08-03 12:55:49 +0800

    Judgment networking

---
+++ AT_4G/src/network.h
@@ -0,0 +1,25 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Hu Ben Yuan<2017603273@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  network.h
+ *    Description:  This Judgment networking include file 
+ *
+ *        Version:  1.0.0(07/30/2023)
+ *         Author:  Hu Ben Yuan <2017603273@qq.com>
+ *      ChangeLog:  1, Release initial version on "07/30/2023 10:33:32 AM"
+ *                 
+ ********************************************************************************/
+
+#ifndef  NETWORK_H
+#define  NETWORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int jud_ppp0_net(void);
+
+#endif
